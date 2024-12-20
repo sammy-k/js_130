6d881202e7802f7ad7c3579bd4090a73c6d1ea1e
Sammy Kanan <samkanan@gmail.com>
2024-12-20 13:28:21 -0600

    update forEach function to take additional args

--- lesson_1/build_forEach.js
+++ lesson_1/build_forEach.js
@@ -1,13 +1,17 @@
 function forEach(array, callback, context) {
   for (let index = 0; index < array.length; index += 1) {
-    callback.call(context, array[index]);
+    callback.call(context, array[index], index, array);
   }
 }
 
 let arr = [1, 2, 3, 4];
 
 // Array.prototype.forEach
-arr.forEach(value => console.log(value * value), arr);
+// arr.forEach(value => console.log(value * value), arr);
 
 // Our forEach function
 forEach(arr, value => console.log(value * value), arr);
+
+forEach(["a", "b", "c"], function(value, index, arr) {
+  console.log(`After ${value} comes ${arr[index + 1]}`);
+});
