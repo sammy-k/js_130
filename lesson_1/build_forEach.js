43e17fbf0e44de92be1f04eb720bd25d3f9dda2a
Sammy Kanan <samkanan@gmail.com>
2024-12-20 13:22:32 -0600

    update forEach function to take execution context into account

--- lesson_1/build_forEach.js
+++ lesson_1/build_forEach.js
@@ -1,13 +1,13 @@
-function forEach(array, callback) {
+function forEach(array, callback, context) {
   for (let index = 0; index < array.length; index += 1) {
-    callback(array[index]);
+    callback.call(context, array[index]);
   }
 }
 
 let arr = [1, 2, 3, 4];
 
 // Array.prototype.forEach
-arr.forEach(value => console.log(value * value));
+arr.forEach(value => console.log(value * value), arr);
 
 // Our forEach function
-forEach(arr, value => console.log(value * value));
+forEach(arr, value => console.log(value * value), arr);
